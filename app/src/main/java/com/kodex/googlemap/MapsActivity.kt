3e5b2e7ae4899f2dbebfab8df075a4d8b40ca145
spiritofenrgy <spiritofenergy220@gmail.com>
2023-01-05 15:15:42 +0300

    Initial commit

---
+++ app/src/main/java/com/kodex/googlemap/MapsActivity.kt
@@ -0,0 +1,41 @@
+package com.kodex.googlemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.kodex.googlemap.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val Moscow = LatLng(55.627328, 37.382313)
+        mMap.addMarker(MarkerOptions().position(Moscow).title("Moscow"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Moscow, 18f))
+    }
+}
